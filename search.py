8479c5c86318281d3a2fafb2e41c3061935b6010
kataa223 <kataa223@gmail.com>
2021-12-12 09:41:59 +0900

    レビュー後の修正

--- search.py
+++ search.py
@@ -2,26 +2,25 @@
 ### これをベースに課題の内容を追記してください
 
 # 検索ソース
-#source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+source = []
 
 ### source.csvを読み込み
-open_file = open('source.csv', encoding='utf-8')
-data = open_file.read()
-source = data.split(',')
-open_file.close()
+def load_charactor_csv():
+    with open("source.csv", "r", encoding="utf-8") as f:
+        return f.read().split(",")
 
 ### sorce、sorce.csvへの追加
 def apeend_source(word):
     source.append(word)
     print("{}をsourceに追加しました".format(word))
     
-    append_file = open('source.csv', 'a', encoding='utf-8')
-    append_file.write(f',{word}')
-    append_file.close()
+    with open("source.csv", "a", encoding="utf-8") as f:
+        f.write(f',{word}')
     print("source.csvに{}を追加しました\n".format(word))
 
 ### 検索ツール
 def search():
+    source = load_charactor_csv()
     word = input("鬼滅の登場人物の名前を入力してください >>> ")
     
     ### ここに検索ロジックを書く
